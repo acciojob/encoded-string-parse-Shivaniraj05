579e904370bd830c2655916fb89730536ed6d1ef
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-05-16 22:52:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const parseCode = (str) => {
   // your code here
-};
+  
+  let splitParts = str.split('0');
+  
+  let allParts = splitParts.filter(parts => parts !=='');
+  
+  return {
+      firstName: allParts[0],
+      lastName: allParts[1],
+      id: allParts[2]
+  };
+  
+}
 
 // Do not change the code below
 const str = prompt("Enter str: ");
